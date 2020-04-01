bfcdf84235e6ddd78fc52ae8ae6925d14a4c1c9d
Bruno Grande <bgrande@sfu.ca>
2020-03-31 20:53:57 -0700

    Enforce Python 3.6 for modutils

--- modutils/setup.py
+++ modutils/setup.py
@@ -7,8 +7,10 @@ setup(
     version="1.0",
     description="Functions for running Snakemake modules",
     url="https://github.com/LCR-BCCRC/lcr-modules",
-      author="Bruno Grande",
-      author_email="bgrande@sfu.ca",
-      license="MIT",
-      py_modules=["modutils"],
-      zip_safe=False)
+    author="Bruno Grande",
+    author_email="bgrande@sfu.ca",
+    license="MIT",
+    py_modules=["modutils"],
+    zip_safe=False,
+    python_requires=">3.6.0",
+)
