9a807b1ea2d3d04b7adfcb1ad6b734cb977d2f40
Bruno Grande <bgrande@sfu.ca>
2020-03-31 20:53:37 -0700

    Update modutils description

--- modutils/setup.py
+++ modutils/setup.py
@@ -2,10 +2,11 @@
 
 from setuptools import setup
 
-setup(name="modutils",
-      version="0.1",
-      description="Shared functions for running Snakemake modules",
-      url="https://github.com/morinlab/pipelines/",
+setup(
+    name="modutils",
+    version="1.0",
+    description="Functions for running Snakemake modules",
+    url="https://github.com/LCR-BCCRC/lcr-modules",
       author="Bruno Grande",
       author_email="bgrande@sfu.ca",
       license="MIT",
