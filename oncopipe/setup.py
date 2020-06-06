451fe2511da3959fd94c5c1639ce5019ad7ebce0
Bruno Grande <bgrande@sfu.ca>
2020-06-05 22:48:40 -0700

    Add setup for oncopipe versioning

--- oncopipe/setup.py
+++ oncopipe/setup.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
 
+# Load modules
+import os
 from setuptools import setup
 
+# Load package info from __version__.py
+about = dict()
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "oncopipe", "__version__.py")) as f:
+    exec(f.read(), about)
+
+# Setup package based on package info
 setup(
-    name="oncopipe",
-    version="1.0",
-    description="Functions for running Snakemake modules",
-    url="https://github.com/LCR-BCCRC/lcr-modules",
-    author="Bruno Grande",
-    author_email="bgrande@sfu.ca",
-    license="MIT",
-    py_modules=["oncopipe"],
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    url=about["__url__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    license=about["__license__"],
+    packages=["oncopipe"],
     install_requires=["pyyaml", "pandas", "snakemake>=5.4",],
     zip_safe=False,
-    python_requires=">3.6.0",
+    python_requires=">=3.6.0",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="snakemake bioinformatics workflows",
 )
