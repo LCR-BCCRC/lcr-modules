326fa25ed6bf757ab4d30d8a262cad01b65b7e85
Bruno Grande <bgrande@sfu.ca>
2020-06-29 00:15:38 -0700

    Parameterize targets

--- demo/run.sh
+++ demo/run.sh
@@ -3,4 +3,7 @@
 # Number of cores set by first command-line argument (default is 4)
 NUM_CORES=${1:-4}
 
-snakemake --cores "${NUM_CORES}" --printshellcmds --use-conda all
+# Default to all targets
+TARGETS=${@:-all}
+
+snakemake --cores "${NUM_CORES}" --printshellcmds --use-conda $TARGETS
