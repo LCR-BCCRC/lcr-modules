f9b1a987e6c500430aea943bbe045bf6c14d666d
Lakshay <lakshay.sethi400@gmail.com>
2021-01-12 15:30:17 -0800

    Script for automatic running

--- demo/capture/lakshay.sh
+++ demo/capture/lakshay.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #./run.sh _picard_qc_all
-./run.sh _bam2fastq_all
-# ./run.sh _manta_all
-# ./run.sh _sequenza_all
-# ./run.sh _lofreq_all
-# ./run.sh _strelka_all
-# ./run.sh _bwa_mem_all
-# ./run.sh _gridss_all
-# ./run.sh _starfish_all
+#./run.sh _bam2fastq_all
+#./run.sh _manta_all
+#./run.sh _sequenza_all
+#./run.sh _lofreq_all    # not  running
+#./run.sh _strelka_all
+./run.sh _bwa_mem_all
+./run.sh _gridss_all
+#./run.sh _starfish_all    # not  running
