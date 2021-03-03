d5cc402667a76c526fcebfc608ce6ae7f9a8790a
Lakshay <lakshay.sethi400@gmail.com>
2021-03-03 11:36:43 -0800

    MAde some changes to the code checker

---
+++ demo/scripts/code.py
@@ -0,0 +1,23 @@
+import sys
+import pkg_resources
+
+
+def checker():
+# Check that the oncopipe dependency is up-to-date. Add all the following lines to any module that uses new features in oncopipe
+    min_oncopipe_version="1.0.11"
+    
+    try:
+        from packaging import version
+    except ModuleNotFoundError:
+        sys.exit("The packaging module dependency is missing. Please install it ('pip install packaging') and ensure you are using the most up-to-date oncopipe version")
+
+# To avoid this we need to add the "packaging" module as a dependency for LCR-modules or oncopipe
+
+    current_version = pkg_resources.get_distribution("oncopipe").version
+    if version.parse(current_version) < version.parse(min_oncopipe_version):
+        print('\x1b[0;31;40m' + f'ERROR: oncopipe version installed: {current_version}' + '\x1b[0m')
+        print('\x1b[0;31;40m' + f"ERROR: This module requires oncopipe version >= {min_oncopipe_version}. Please update oncopipe in your environment" + '\x1b[0m')
+        sys.exit("Instructions for updating to the current version of oncopipe are available at https://lcr-modules.readthedocs.io/en/latest/ (use option 2)")
+
+# End of dependency checking section 
+print("hs")
