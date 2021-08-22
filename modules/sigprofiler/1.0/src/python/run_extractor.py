4aa200383226afafce35e175e237cf1b587dd3d7
ppararaj <ppararaj@gmail.com>
2021-08-22 14:11:25 -0700

    Added comments and argparse help.

--- modules/sigprofiler/1.0/src/python/run_extractor.py
+++ modules/sigprofiler/1.0/src/python/run_extractor.py
@@ -2,9 +2,10 @@ import argparse
 from SigProfilerExtractor import sigpro as sig
 import os
 
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1" # ulimit fix
 
 def main():
+    """ Performs NMF decomposition with provided matrix """
     args = parse_arguments()
     sig.sigProfilerExtractor(input_type = 'matrix', \
                              output = args.output, \
@@ -24,17 +25,17 @@ def main():
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_data')
-    parser.add_argument('output')
-    parser.add_argument('ref')
-    parser.add_argument('context_type', default = '96,DINUC,ID')
-    parser.add_argument('exome', default = False, type = bool)
-    parser.add_argument('minimum_signatures', default = 1, type = int)
-    parser.add_argument('maximum_signatures', default = 25, type = int)
-    parser.add_argument('nmf_replicates', default = 100 , type = int)
-    parser.add_argument('matrix_normalization', default = 'gmm')
-    parser.add_argument('nmf_init', default = 'nndsvd_min')
-    parser.add_argument('cpu', default = -1, type = int)
+    parser.add_argument('input_data', help = 'Path to matrix that will undergo NMF.')
+    parser.add_argument('output', help = 'Path to output directory.')
+    parser.add_argument('ref', help = 'Reference build of matrix.')
+    parser.add_argument('context_type', default = '96,DINUC,ID', help = 'A string of mutaion context name/names separated by comma (","). The items in the list defines the mutational contexts to be considered to extract the signatures. The default value is "96,DINUC,ID", where "96" is the SBS96 context, "DINUC" is the DINUCLEOTIDE context and ID is INDEL context.')
+    parser.add_argument('exome', default = False, type = bool, = 'Defines if the exomes will be extracted. The default value is "False".')
+    parser.add_argument('minimum_signatures', default = 1, type = int, help = "The minimum number of signatures to be extracted. The default value is 1.")
+    parser.add_argument('maximum_signatures', default = 25, type = int, help = "The maximum number of signatures to be extracted. The default value is 25.")
+    parser.add_argument('nmf_replicates', default = 100 , type = int, help = "The number of iteration to be performed to extract each number signature. The default value is 100.")
+    parser.add_argument('matrix_normalization', default = 'gmm', help = 'Method of normalizing the genome matrix before it is analyzed by NMF. Default is value is "gmm". Other options are, "log2", "custom" or "none".')
+    parser.add_argument('nmf_init', default = 'nndsvd_min', help = "The initialization algorithm for W and H matrix of NMF. Options are 'random', 'nndsvd', 'nndsvda', 'nndsvdar' and 'nndsvd_min'. Default is 'nndsvd_min'.")
+    parser.add_argument('cpu', default = -1, type = int, help = 'Number of threads to use. Default use all threads.')
     
     return(parser.parse_args())
 
--- modules/sigprofiler/1.0/src/python/run_generator.py
+++ modules/sigprofiler/1.0/src/python/run_generator.py
@@ -3,8 +3,11 @@ import os.path
 from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as mg
 
 def main():
+    """ Generates matrix from MAF file """
     args = parse_arguments()
-    outdir = os.path.dirname(args.maf)
+    outdir = os.path.dirname(args.maf) # output made in MAF directory
+
+    # Generates matrix from MAF found in outdir
     matrices = mg.SigProfilerMatrixGeneratorFunc(args.project, args.ref, \
                                                  outdir, tsb_stat = True)
     return()
