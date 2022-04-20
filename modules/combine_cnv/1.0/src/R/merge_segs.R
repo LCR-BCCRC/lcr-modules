2b1ec791e9faf733e1a12d314b9da1d0d2bdb386
Kdreval <k.dreval@gmail.com>
2022-04-20 13:55:30 -0700

    functionality improvement: replace merging script

---
+++ modules/combine_cnv/1.0/src/R/merge_segs.R
@@ -0,0 +1,53 @@
+#!/usr/bin/env Rscript
+#
+
+
+suppressWarnings(suppressPackageStartupMessages({
+
+  message("Loading packages...")
+
+  required_packages <- c(
+    "tidyverse"
+  )
+
+  installed_packages <- rownames(installed.packages())
+
+  for (pkg in required_packages){
+    if (!pkg %in% installed_packages) {
+      message(paste0("Installing ", pkg, "..."))
+      install.packages(pkg, repos = "https://mirror.rcg.sfu.ca/mirror/CRAN/")
+    }
+    library(pkg, character.only = TRUE)
+  }
+}))
+
+
+# Read all individual seg files ----------------------------------------------
+message("Loading data from individual seg files ...")
+
+files <- snakemake@input[[1]]
+
+# read individual files and keep file paths in the column filename
+data =
+tibble(filename = files) %>% # create a data frame
+  # holding the file names
+  mutate(file_contents = map(filename,          # read files into
+                             ~ read_tsv(files, col_types = cols())) # a new data column
+  ) %>%
+  unnest(cols = c(file_contents))
+
+# strip file paths for the final seg file
+output <- data %>%
+  select(-filename) %>%
+  as.data.frame
+
+# this is the file path of all individual segs used in merging
+contents <- data %>%
+  select(filename) %>%
+  unique %>%
+  as.data.frame
+
+# Output data ------------------------------------------------------
+message("Writing final outputs ...")
+write_tsv(output, snakemake@output[[1]])
+write_tsv(contents, snakemake@output[[2]], col_names = FALSE)
