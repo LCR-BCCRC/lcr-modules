002159e9ba9c5908e2455f2bc98476cfba3b3496
Kdreval <k.dreval@gmail.com>
2022-04-20 18:43:23 -0700

    cleanup: tested version

--- modules/cnv_master/1.0/src/R/merge_segs.R
+++ modules/cnv_master/1.0/src/R/merge_segs.R
@@ -7,7 +7,7 @@ suppressWarnings(suppressPackageStartupMessages({
   message("Loading packages...")
 
   required_packages <- c(
-    "tidyverse"
+    "readr", "dplyr", "tidyr", "purrr"
   )
 
   installed_packages <- rownames(installed.packages())
@@ -25,20 +25,21 @@ suppressWarnings(suppressPackageStartupMessages({
 # Read all individual seg files ----------------------------------------------
 message("Loading data from individual seg files ...")
 
-files <- snakemake@input[[1]]
+files <- snakemake@input[]
 
 # read individual files and keep file paths in the column filename
 data =
-tibble(filename = files) %>% # create a data frame
+tibble(filename = files$seg_file) %>% # create a data frame
   # holding the file names
   mutate(file_contents = map(filename,          # read files into
-                             ~ read_tsv(files, col_types = cols())) # a new data column
+                             ~ read_tsv(files$seg_file, col_types = cols())) # a new data column
   ) %>%
   unnest(cols = c(file_contents))
 
 # strip file paths for the final seg file
 output <- data %>%
   select(-filename) %>%
+  distinct %>%
   as.data.frame
 
 # this is the file path of all individual segs used in merging
