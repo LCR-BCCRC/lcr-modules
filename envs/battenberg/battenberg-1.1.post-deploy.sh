e0bc299e0ae665634033d0cb97ec3798368c976e
lkhilton <laura.k.hilton@gmail.com>
2024-08-09 16:43:00 -0700

    Patches to new battenberg conda envs

--- envs/battenberg/battenberg-1.1.post-deploy.sh
+++ envs/battenberg/battenberg-1.1.post-deploy.sh
@@ -3,4 +3,4 @@ set -o pipefail
 set -e # Exit on error
 
 R --vanilla -q -e 'devtools::install_github("morinlab/ascat/ASCAT", dependencies = TRUE, upgrade = "never")'
-R --vanilla -q -e 'devtools::install_github("morinlab/battenberg", dependencies = TRUE, upgrade = "never")'
+R --vanilla -q -e 'devtools::install_github("morinlab/battenberg", dependencies = FALSE, upgrade = "never")'
