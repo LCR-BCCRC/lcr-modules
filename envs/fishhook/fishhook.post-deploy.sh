0f3f659527cd3cf09ea91407d9f1f97b7ff4e225
gillissierra <gillissierra@gmail.com>
2024-12-11 08:45:55 -0800

    rm test file from post deploy

--- envs/fishhook/fishhook.post-deploy.sh
+++ envs/fishhook/fishhook.post-deploy.sh
@@ -2,6 +2,6 @@
 set -o pipefail
 set -e # Exit on error
 
-R --vanilla -q -e 'devtools::install_github("mskilab/gUtils", dependencies = FALSE, upgrade = "never")' > /home/sgillis/fishhook_deploy.txt
-R --vanilla -q -e 'devtools::install_github("mskilab/gTrack", dependencies = FALSE, upgrade = "never")' >> /home/sgillis/fishhook_deploy.txt
-R --vanilla -q -e 'devtools::install_github("mskilab/fishHook", dependencies = FALSE, upgrade = "never")' >> /home/sgillis/fishhook_deploy.txt
+R --vanilla -q -e 'devtools::install_github("mskilab/gUtils", dependencies = FALSE, upgrade = "never")'
+R --vanilla -q -e 'devtools::install_github("mskilab/gTrack", dependencies = FALSE, upgrade = "never")'
+R --vanilla -q -e 'devtools::install_github("mskilab/fishHook", dependencies = FALSE, upgrade = "never")'
