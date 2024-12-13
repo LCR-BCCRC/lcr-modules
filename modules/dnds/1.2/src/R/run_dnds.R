ade8d1b33c3fbc7be330448a8c3b1238caf3d8b4
Kdreval <k.dreval@gmail.com>
2024-12-13 15:49:47 -0800

    document and style

--- modules/dnds/1.2/src/R/run_dnds.R
+++ modules/dnds/1.2/src/R/run_dnds.R
@@ -29,8 +29,8 @@ maf = as.data.frame(maf)
 
 if (grepl("38", maf$NCBI_Build[1])) {
     cat("Will run using the hg38 reference.\n")
-    load(snakemake@params[[3]]) # This will load the RefCDS from the path at config
-    load(snakemake@params[[4]]) # This will load the covs from the path at config
+    load(snakemake@params[["hg38_reference"]]) # This will load the RefCDS from the path at config
+    load(snakemake@params[["hg38_covariates"]]) # This will load the covs from the path at config
     use_this_reference <- RefCDS
     use_this_cv <- covs
 }else{
@@ -41,25 +41,26 @@ if (grepl("38", maf$NCBI_Build[1])) {
 
 # Run the dNdS
 # should we restrict to target genes?
-if(file.exists(snakemake@params[[1]])){
-    print(paste("Restricting analysis to target genes specified in", snakemake@params[[1]]))
+if(file.exists(snakemake@params[["target_genes"]])){
+    print(paste("Restricting analysis to target genes specified in", snakemake@params[["target_genes"]]))
     # Read the target genes file
     target_genes = read.table(
-        file = snakemake@params[[1]],
+        file = snakemake@params[["target_genes"]],
         sep = '\t',
         header = FALSE
     )
+    # In this case we are not supplying the covariates since when
+    # a gene list is supplied, the covariates are ignored
     dndsout = dndscv(
         maf,
         gene_list=target_genes$V1,
-        max_muts_per_gene_per_sample=snakemake@params[[2]],
-        refdb = use_this_reference,
-        cv = use_this_cv
+        max_muts_per_gene_per_sample=snakemake@params[["max_muts_per_gene_per_sample"]],
+        refdb = use_this_reference
     )
 }else{
     dndsout = dndscv(
         maf,
-        max_muts_per_gene_per_sample=snakemake@params[[2]],
+        max_muts_per_gene_per_sample=snakemake@params[["max_muts_per_gene_per_sample"]],
         refdb = use_this_reference,
         cv = use_this_cv
     )
@@ -70,7 +71,7 @@ sig_genes = as.data.frame(dndsout$sel_cv)
 
 write.table(
     sig_genes,
-    file=snakemake@output[[1]],
+    file=snakemake@output[["dnds_sig_genes"]],
     quote=FALSE,
     sep='\t',
     row.names = FALSE)
@@ -80,7 +81,7 @@ annotmuts = as.data.frame(dndsout$annotmuts)
 
 write.table(
     annotmuts,
-    file=snakemake@output[[2]],
+    file=snakemake@output[["annotmuts"]],
     quote=FALSE,
     sep='\t',
     row.names = FALSE)
