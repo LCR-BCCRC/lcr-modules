acac6d068c69e53ec22d8dd1c884c19f91dd594c
lkhilton <laura.k.hilton@gmail.com>
2025-01-27 11:45:32 -0800

    Make runnable with/without SV and CNV

--- modules/dlbclass/1.0/src/sv2gsm.R
+++ modules/dlbclass/1.0/src/sv2gsm.R
@@ -16,6 +16,7 @@ sv_files <- snakemake@input[["sv"]]
 
 # Load sample set file
 samples <- read.delim(samples, sep = "\t")
+samples <- samples[[1]]
 
 # Load SV file
 svs <- lapply(sv_files, read.delim, sep = "\t")
@@ -37,13 +38,38 @@ svs$SV.BCL6 <- gsub(neg_value, 0, gsub(pos_value, 3, svs$SV.BCL6))
 svs$SV.BCL2 <- gsub(neg_value, 0, gsub(pos_value, 3, svs$SV.BCL2))
 svs$SV.MYC <- gsub(neg_value, 0, gsub(pos_value, 3, svs$SV.MYC))
 
-# Pivot to feature rows and sample columns
+# Match df to input samples
 rownames(svs) <- svs$sample_id
 svs <- svs[, 2:4]
-samples <- samples[[1]][samples[[1]] %in% rownames(svs)]
-svs <- data.frame(t(svs[samples, ]), check.names = FALSE)
+samples_keep <- samples[samples %in% rownames(svs)]
+message(paste0("Number of samples in SV file: ", length(samples_keep)))
+
+samples_missing <- samples[!samples %in% samples_keep]
+message(paste0("Number of samples missing from SV file: ", length(samples_missing)))
+
+# Make a matrix for missing samples
+sv_empty <- data.frame(matrix(0, ncol = 1, nrow = 3))
+rownames(sv_empty) <- colnames(svs)
+if (length(samples_missing) > 0) {
+    sv_empty <- data.frame(matrix(0, ncol = length(samples_missing), nrow = 3))
+    colnames(sv_empty) <- samples_missing
+    rownames(sv_empty) <- colnames(svs)
+}
+message(paste0("Dimensions of sv_empty: ", paste0(dim(sv_empty), collapse = "x")))
+
+# Convert non-empty SV DF to GSM format
+if (length(samples_keep) > 0) {
+    svs <- data.frame(t(svs[samples_keep, ]), check.names = FALSE)
+    svs <- cbind(sv_empty, svs)
+} else {
+    (svs <- sv_empty)
+}
+message(paste0("Dimensions of svs: ", paste0(dim(svs), collapse = "x")))
+
+# Final formatting steps
 svs$classifier_name <- rownames(svs)
 svs <- svs[, c("classifier_name", samples)]
+
 
 # Write to file
 write.table(svs, snakemake@output[["sv_gsm"]], row.names = FALSE, quote = FALSE, sep = "\t")
