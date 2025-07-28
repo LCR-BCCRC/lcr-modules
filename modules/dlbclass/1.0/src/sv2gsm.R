65182a25fe4b5090ca2118a5c9f1571d5ecc4644
lkhilton <laura.k.hilton@gmail.com>
2025-07-28 14:32:57 -0700

    Bug fixes in sv2gsm.R

--- modules/dlbclass/1.0/src/sv2gsm.R
+++ modules/dlbclass/1.0/src/sv2gsm.R
@@ -21,7 +21,7 @@ samples <- samples[[1]]
 # Load SV file
 svs <- lapply(sv_files, read.delim, sep = "\t")
 svs <- do.call(rbind, svs)
-svs <- svs[!duplicated(svs[, 1]), ]
+svs <- unique(svs)
 
 # Confirm correct dimensions
 if (length(colnames(svs)) != 4) {
@@ -29,9 +29,7 @@ if (length(colnames(svs)) != 4) {
 }
 
 # Convert column names to match gsm requirements
-colnames(svs) <- gsub(".*(BCL2|BCL6|MYC).*", "\\1", toupper(colnames(svs)))
-colnames(svs) <- paste0("SV.", colnames(svs))
-colnames(svs)[1] <- "sample_id"
+colnames(svs) <- gsub(".*(BCL2|BCL6|MYC).*", "SV.\\1", toupper(colnames(svs)))
 
 # Convert SV values to GSM values (0 for negative, 3 for positive)
 svs$SV.BCL6 <- gsub(neg_value, 0, gsub(pos_value, 3, svs$SV.BCL6))
@@ -39,8 +37,8 @@ svs$SV.BCL2 <- gsub(neg_value, 0, gsub(pos_value, 3, svs$SV.BCL2))
 svs$SV.MYC <- gsub(neg_value, 0, gsub(pos_value, 3, svs$SV.MYC))
 
 # Pivot to feature rows and sample columns
-rownames(svs) <- svs$sample_id
-svs <- svs[, 2:4]
+rownames(svs) <- svs$SAMPLE_ID
+svs <- svs[, grepl("SV", colnames(svs))]
 samples <- samples[samples %in% rownames(svs)]
 svs <- data.frame(t(svs[samples, ]), check.names = FALSE)
 svs$classifier_name <- rownames(svs)
