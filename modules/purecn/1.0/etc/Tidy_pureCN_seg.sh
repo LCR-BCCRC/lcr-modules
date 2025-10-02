55465f641a43fffa00a3e2c5bd0399ed39ae019c
gillissierra <gillissierra@gmail.com>
2025-10-02 13:57:30 -0700

    pureCN fix tidy script

--- modules/purecn/1.0/etc/Tidy_pureCN_seg.sh
+++ modules/purecn/1.0/etc/Tidy_pureCN_seg.sh
@@ -4,7 +4,7 @@
 if [ "$1" == "-h" ] ; then
     echo -e "Using seg files generated from pureCN and converting them to a format for downstream reformatting.
 Usage: `basename $0` -i <seg file> -o <output seg file>
-    <seg file>: input seg file - type the path; 
+    <seg file>: input seg file - type the path;
     <out file>: output seg file - type the path;
     <sample_id>: sample_id name "
     exit 0
@@ -24,10 +24,10 @@ do
 done
 
 echo -e "Running... $seg"
-echo -e "ID\tchrom\tstart\tend\tnum.mark\tseg.mean" > $out
+echo -e "ID\tchrom\tstart\tend\tnum.mark\tseg.mean\tC" > $out
 
 echo $seg
-awk '(NR>1)' $seg | awk -v sample="$sample_id" '{print sample"\tchr"$2"\t"int($3)"\t"int($4)"\t"$5"\t"log($7)/log(2)-1}' | sed 's/chr23/chrX/g' | sed 's/chr24/chrY/g' | sed 's/chrchr/chr/g' | sed 's/-inf/-2/g' | sort -k1,1 -k2,2 -k3,3n -k4,4n -V | awk '($4 > $3)' |  awk '$3+1!=$4 {print}' >> $out
+awk '(NR>1)' $seg | awk -v OFS="\t" -v sample="$sample_id" '{print sample,"chr"$2,int($3), int($4),$5,$6,$7}' | sed 's/chr23/chrX/g' | sed 's/chr24/chrY/g' | sed 's/chrchr/chr/g' | sed 's/-inf/-2/g' | sort -k1,1 -k2,2 -k3,3n -k4,4n -V | awk '($4 > $3)' | awk -v OFS="\t" '$3+1!=$4 {print}' >> $out
 
 echo -e "Finished writing... $out"
 
